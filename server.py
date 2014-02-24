ba2fc8db54bdefe24fead187fc0bab38b65a08b1
AIeq <pietari.kokko@aalto.fi>
2014-02-25 00:29:10 +0200

    Create server.py
    Ensimmäinen versio serveristä. Palauttaa WebSocket-viestin takaisin lähettäneelle sivulle.

---
+++ server.py
@@ -0,0 +1,30 @@
+from flask import Flask, request, render_template, views
+from geventwebsocket.handler import WebSocketHandler
+from gevent.pywsgi import WSGIServer
+
+app = Flask(__name__) 
+
+@app.route('/api')
+def api():
+	if request.environ.get('wsgi.websocket'):
+		ws = request.environ['wsgi.websocket']
+		while True:
+			message = ws.receive()
+			ws.send(message)
+	return
+
+class Aula(views.MethodView):
+	def get(self):
+		return render_template('aula.html')
+
+class Peli(views.MethodView):
+	def get(self):
+		return render_template('peli.html')
+
+app.add_url_rule('/', view_func=Aula.as_view('index'))
+app.add_url_rule('/peli.html', view_func=Peli.as_view('main'))
+app.debug = False
+
+if __name__ == '__main__':
+	http_server = WSGIServer(('',5000), app, handler_class=WebSocketHandler)
+	http_server.serve_forever()
