f96243ca7260c91a434fa2408e1608db456dc830
AIeq <pietari.kokko@aalto.fi>
2014-03-03 17:15:44 +0200

    Update server.py
    Jaetaan viestit kaikille pelaajille.

--- server.py
+++ server.py
@@ -2,15 +2,24 @@ from flask import Flask, request, render_template, views
 from geventwebsocket.handler import WebSocketHandler
 from gevent.pywsgi import WSGIServer
 
+
+PELAAJAT = []
 app = Flask(__name__) 
+
 
 @app.route('/api')
 def api():
 	if request.environ.get('wsgi.websocket'):
-		ws = request.environ['wsgi.websocket']
+		ws = request.environ['wsgi.websocket'] 
+		PELAAJAT.append(ws)
 		while True:
 			message = ws.receive()
-			ws.send(message)
+			for websocket in PELAAJAT: 
+				try :
+					websocket.send(message)
+				except :
+					# connection lost
+					PELAAJAT.remove(websocket)
 	return
 
 class Aula(views.MethodView):
@@ -26,5 +35,6 @@ app.add_url_rule('/peli.html', view_func=Peli.as_view('main'))
 app.debug = False
 
 if __name__ == '__main__':
+
 	http_server = WSGIServer(('',5000), app, handler_class=WebSocketHandler)
 	http_server.serve_forever()
